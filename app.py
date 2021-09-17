16ca56764b0a42f9a493019425aeac4bec2150f6
Krishna Kumar Selvam <krishnakumarselvam@Krishnas-MacBook-Pro.local>
2021-09-17 00:25:12 -0700

    implement

--- app.py
+++ app.py
@@ -63,8 +63,8 @@ class Show(db.Model):
     __tablename__ = 'Show'
     id = db.Column(db.Integer, primary_key=True)
     venue_id = db.Column(db.Integer, db.ForeignKey('Venue.id'), nullable=True)
-    artist_id = db.Column(db.Integer, db.ForeignKey('Artist.id'), nullable=True)b
-
+    artist_id = db.Column(db.Integer, db.ForeignKey('Artist.id'), nullable=True)
+    start_time = db.Column(db.DateTime(timezone=True), nullable=False)
 # TODO Implement Show and Artist models, and complete all model relationships and properties, as a database migration.
 
 #----------------------------------------------------------------------------#
